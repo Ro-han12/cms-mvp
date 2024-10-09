181e4a65e7877a3733b90c46b4f39a6e9e43f5b4
rohan <rohannair2939@gmail.com>
2024-10-09 10:05:34 +0530

    ui changes

--- src/app/[[...sign-in]]/page.tsx
+++ src/app/[[...sign-in]]/page.tsx
@@ -28,8 +28,8 @@ const LoginPage = () => {
           className="bg-white p-12 rounded-md shadow-2xl flex flex-col gap-2"
         >
           <h1 className="text-xl font-bold flex items-center gap-2">
-            <Image src="/logo.png" alt="" width={24} height={24} />
-            SchooLama
+            <Image src="/logo.png" alt="" width={100} height={100} />
+            cms
           </h1>
           <h2 className="text-gray-400">Sign in to your account</h2>
           <Clerk.GlobalError className="text-sm text-red-400" />
